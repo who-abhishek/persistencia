93029e815a9cf320af7612ee99e71136b9409002
an-rahulpandey <abhishekyadav28@outlook.com>
2022-05-26 23:49:05 +0530

    Register user api

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const {  body, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 /**
  * @route  GET api/user
@@ -10,20 +13,53 @@ const {  body, validationResult } = require("express-validator");
 router.post(
   "/",
   [
-    body('name', 'Name is required').notEmpty(),
-    body('email', 'Please enter a valid email').isEmail(),
+    body("name", "Name is required").notEmpty(),
+    body("email", "Please enter a valid email").isEmail(),
     // password must be at least 5 chars long
-    body('password','Please enter a password with more than 6 characters').isLength({ min: 5 }),
+    body(
+      "password",
+      "Please enter a password with more than 6 characters"
+    ).isLength({ min: 5 }),
   ],
-  (req, res, next) => {
+  async (req, res, next) => {
     console.log("user route line 10", req.body);
-     // Finds the validation errors in this request and wraps them in an object with handy functions
-     const errors = validationResult(req);
-     if (!errors.isEmpty()) {
-       return res.status(400).json({ errors: errors.array() });
-     }
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    res.send("users route");
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exist" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User registered');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
